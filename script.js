a7237eb466f32b2df48c535cbf7dc1a5e4c04a28
Ian <ianmcm.89@gmail.com>
2021-10-26 16:44:25 -0400

    added pop up window functionality

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetchInitialCrew(fetchAdditionalCrew);
-    createEventListener('#crew-form', 'submit', checkFormInputs);
-    createEventListener('button.launch-btn', 'click', handleLaunch);
+    fetchAllCrewMembers(staticCrew);
+
+    document.querySelector('#crew-form').addEventListener('submit', checkFormInputs);
+    document.querySelector('#launch-btn').addEventListener('click', handleLaunchButton);
 })
 
-const initialCrew = ['Leela', 'Fry', 'Bender', 'Amy', 'Zoidberg', 'Hermes', 'Scruffy'];
+const staticCrew = ['Leela', 'Fry', 'Bender', 'Amy', 'Zoidberg', 'Hermes', 'Scruffy'];
 
 const planets = {
     1: {
@@ -20,7 +21,7 @@ const planets = {
         client: 'Nibblonians',
         },
     4: {
-        name: 'Globetrotter Homeworld',
+        name: 'the Globetrotter Homeworld',
         client: 'Globetrotters',
         },
     5: {
@@ -49,52 +50,45 @@ const planets = {
         },
 }
 
-let crewCards = [];
+let crewSelected = [];
+
+const fetchAllCrewMembers = crew => crew.forEach(crewMember => fetchStaticCrew(crewMember)) & fetchCustomCrew();
 
 //Fetch JSON data for initial crew:
-const fetchInitialCrew = (cbFunction) => {
-    initialCrew.forEach(crewMember => {
-        fetch(`http://futuramaapi.herokuapp.com/api/v2/characters?search=${crewMember}`)
-        .then(resp => resp.json())
-        .then(jsonCharacter => {
-            jsonCharacter[0].Name = crewMember;
-            createAvailableList(jsonCharacter[0]);
-        })
-        .catch(err => console.error(err))
-    })
-    cbFunction();
-}
-
-//Fetch JSON data for custom crew:
-const fetchAdditionalCrew = () => {
-    fetch('http://localhost:3000/characters')
+const fetchStaticCrew = (crewMember) => {
+    fetch(`http://futuramaapi.herokuapp.com/api/v2/characters?search=${crewMember}`)
     .then(resp => resp.json())
-    .then(json => json.forEach(crewMember => createAvailableList(crewMember)))
+    .then(json => {
+        json[0].Name = crewMember;
+        createListItem(json[0]);
+    })
     .catch(err => console.error(err))
 }
 
-//Append given element parameter to a given parent parameter:
+//Fetch JSON data for custom crew:
+const fetchCustomCrew = () => {
+    fetch('http://localhost:3000/characters')
+    .then(resp => resp.json())
+    .then(json => json.forEach(crewMember => createListItem(crewMember)))
+    .catch(err => console.error(err))
+}
+
 const appendElement = (parent, child) => document.querySelector(parent).appendChild(child);
 
-const createEventListener = (element, action, cbFunction) => document.querySelector(element).addEventListener(action, (e) => cbFunction(e));
-
-//Check if form inputs are empty:
-const checkFormInputs = (e) => {
+const checkFormInputs = e => {
     e.preventDefault();
-    
+
     let inputs = {
         name: e.target.name.value,
         image: e.target.image.value,
         species: e.target.species.value,
     }
 
-    Object.values(inputs).includes('') ? throwSubmitError() : handleSubmit(e, inputs);
+    Object.values(inputs).includes('') ? emptyInputMessage() : handleSubmit(e, inputs);
 }
 
-//Throws message if any input fields are empty:
-const throwSubmitError = () => alert('You need to fill all input fields');
+const emptyInputMessage = () => createPopUpWindow('"You need to fill out ALL the Crew-Bobulator\'s input fields...you knucklehead."');
 
-//Submit event handler function:
 const handleSubmit = (e, inputs) => {
     let newCharacterObj = {
         id: '',
@@ -104,25 +98,34 @@ const handleSubmit = (e, inputs) => {
         likes: 0,
     }
 
-    createAvailableList(newCharacterObj);
-    
     postCrewMember(newCharacterObj);
 
     clearFormInputs(e, inputs);
 }
 
-//Reset form inputs after submit:
-const clearFormInputs = (e, inputs) => Object.keys(inputs).forEach(input => e.target[input].value = '');
+const postCrewMember = crewMember => {
+    fetch(`http://localhost:3000/characters`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        body: JSON.stringify(crewMember),
+    })
+    .then(resp => resp.json())
+    .then(crewMember => createListItem(crewMember))
+    .catch(err => console.error(err))
+}
 
-//Add click event listener to created li and append to DOM:
-const createAvailableList = (crewMember) => {
+const createListItem = (crewMember) => {
     let li = createNewLi(crewMember);
 
     appendElement('#available-list', li);
 }
 
-//Create a new li for provided crewmember:
-const createNewLi = (crewMember) => {
+const clearFormInputs = (e, inputs) => Object.keys(inputs).forEach(input => e.target[input].value = '');
+
+const createNewLi = crewMember => {
     let li = document.createElement('li');
 
     li.className = 'character-li';
@@ -133,20 +136,19 @@ const createNewLi = (crewMember) => {
     return li;
 }
 
-//li click event handler:
 const handleLiClick = (crewMember, li) => {
-    crewCards.length < 5? updateCrewList(crewMember, li) : alert('You dingbat! The Planet Express ship already has a full crew!');
+    if (crewSelected.length < 5) return updateCrewList(crewMember, li);
+    createPopUpWindow('"The Planet Express ship already has a full crew, you dingbat! Don\'t you have a delivery to make?...or do you? My memory isn\'t quite what it used to be..."');
 }
 
 const updateCrewList = (crewMember, li) => {
-    crewCards.push(crewMember.Name);
+    crewSelected.push(crewMember.Name);
     
     createCharacterCard(crewMember, li);
 
-    li.className = 'selected';
+    li.className = 'li-selected';
 }
 
-//Create a div card for each given character:
 const createCharacterCard = (crewMember, li) => {
     let card = document.querySelector('div.empty-character-card');
 
@@ -159,7 +161,6 @@ const createCharacterCard = (crewMember, li) => {
         <img src="${crewMember.PicUrl}" class='character-image' />
         <p>${crewMember.Species}</p>
         <div class='footer-div'>
-            <button class="like-btn">Like</button>
             <button class="fire-btn">Fire</button>
         </div>
     `;
@@ -172,28 +173,24 @@ const createCardEventListners = (card, crewMember, li) => {
     card.querySelector('button.fire-btn').addEventListener('click', (e) => handleFireButton(e, crewMember, li));
 }
 
-//Fire button event handler:
 const handleFireButton = (e, crewMember, li) => {
-    if (initialCrew.includes(crewMember.Name)) {
-        alert(`You can\'t fire ${crewMember.Name}`);
+    if (staticCrew.includes(crewMember.Name)) {
+        createPopUpWindow(`You can\'t fire ${crewMember.Name}, you nincumpoop! Planet Express shareholders are off limits...confounded bureaucracy...`);
     } else {
         removeCharacterCard(e, crewMember);
 
-        console.log(crewMember.id);
         deleteCrewMember(crewMember);
     
         li.remove();
     }
 }
 
-//Remove button event handler:
 const handleRemoveButton = (e, crewMember, li) => {
     removeCharacterCard(e, crewMember);
 
     li.className = 'character-li';
 }
 
-//Remove character div card from table:
 const removeCharacterCard = (e, crewMember) => {
     e.target.parentElement.parentNode.remove();
 
@@ -207,27 +204,12 @@ const createEmptyCard = () => {
 
     emptyCard.className = 'empty-character-card';
 
-    appendElement('#crew-cards', emptyCard);
+    appendElement('#character-cards', emptyCard);
 }
 
-//Remove character from crewCards array:
-const updateCrewCards = (crewMember) => crewCards = crewCards.filter(character => character !== crewMember.Name);
+const updateCrewCards = crewMember => crewSelected = crewSelected.filter(character => character !== crewMember.Name);
 
-const postCrewMember = (crewMember) => {
-    fetch(`http://localhost:3000/characters`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        body: JSON.stringify(crewMember),
-    })
-    .then(resp => resp.json())
-    .then(crewMember => console.log(crewMember))
-    .catch(err => console.error(err))
-}
-
-const deleteCrewMember = (crewMember) => {
+const deleteCrewMember = crewMember => {
     fetch(`http://localhost:3000/characters/${crewMember.id}`, {
         method: 'Delete',
     })
@@ -236,31 +218,58 @@ const deleteCrewMember = (crewMember) => {
     .catch(err => console.error(err))
 }
 
-//Launch button event handler function
-const handleLaunch = () => {
-    let warningMessage = `The Planet Express ship needs ${5 - crewCards.length} more crew members before it can take off!`;
+//Launch Button functionality:
 
-    document.querySelector('#launch-window').style.display = 'flex';
-
-    //crewCards.length < 5 ? alert(warningMessage) : alert(generateLaunchAlert());
+const handleLaunchButton = () => {
+    generateMessage();
 }
 
-//Generates a success or failure message when launch button is clicked:
-const generateLaunchAlert = () => {
-    let currentCrew = `${crewCards.slice(0, 4).join(', ')} and ${crewCards.slice(4)}`;
+
+const createPopUpWindow = (message, imageURL = 'images/farnsworth-sad.png') => {
+    let popUpWindow = document.querySelector('#pop-up-window-container');
+
+    popUpWindow.style.display = 'flex';
+    
+    document.querySelector('#pop-up-window-container').innerHTML = `
+        <div id='pop-up-window'>
+            <img id='farnsworth-image' src=${imageURL}>
+            <div id='message-container'>
+                <p id='farnsworth-message'>${message}</p>
+                <button id='confirm-btn'>CONFIRM</button>
+            </div>
+        </div>
+    `
+
+    document.querySelector('#confirm-btn').addEventListener('click', () => popUpWindow.style.display = 'none');
+}
+
+const generateMessage = () => {
+    if (crewSelected.length < 5) return createPopUpWindow(`"The Planet Express ship needs ${5 - crewSelected.length} more crew before it can depart..."`); createLaunchMessage();
+}
+
+const createLaunchMessage = () => {
+    let currentCrew = `${crewSelected.slice(0, 4).join(', ')} and ${crewSelected.slice(4)}`;
 
     let planet = generateRandomPlanet();
 
     return generateRandomMessage(currentCrew, planet);
 }
 
-//Returns a random planet:
 const generateRandomPlanet = () => planets[Math.ceil(Math.random() * 10)];
 
-//Returns a random message:
 const generateRandomMessage = (currentCrew, planet) => {
-    let successMessage = `Hoozah! ${currentCrew} succesfully delivered the package to ${planet.name}. The ${planet.client} are quite pleased!`;
-    let failMessage = `Oh bother! It seems ${currentCrew} failed to deliver the package to ${planet.name}. The ${planet.client} wont be happy about this...`;
+    if (Math.random() >= 0.5) return handleDeliverySuccess(currentCrew, planet);
+    handleDeliveryFailure(currentCrew, planet);
+}
 
-    return Math.random() > 0.5 ? successMessage : failMessage;
+const handleDeliverySuccess = (currentCrew, planet) => {
+    let successMessage = `"Hoozah! ${currentCrew} succesfully delivered the package to ${planet.name}. The ${planet.client} are quite pleased!"`;
+
+    createPopUpWindow(successMessage, 'images/farnsworth-happy.png');
+}
+
+const handleDeliveryFailure = (currentCrew, planet) => {
+    let failureMessage = `"Oh bother! It seems ${currentCrew} failed to deliver the package to ${planet.name}. The ${planet.client} wont be happy about this..."`;
+
+    createPopUpWindow(failureMessage);
 }
